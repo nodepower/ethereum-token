f004a3537fef49828328f25e888ef06f861f8fb0
Kirill Varlamov <k.varlamov@brain4net.com>
2018-01-13 16:57:06 +0300

    added rinkeby configuration

--- truffle.js
+++ truffle.js
@@ -7,6 +7,12 @@ module.exports = {
       port: 9545,
       network_id: "*", // Match any network id,
       gas: 5000000
+    },
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id,
+      gas: 5000000
     }
   }
 };
